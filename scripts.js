8e41052752b2c5e26da2cffd472a56f9c1f6d8e2
DavicaChambers <davicachambers73@gmail.com>
2021-10-24 10:48:53 -0500

    Added 6 pictures and modified files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 /* Add your JavaScript to this file */
+
+const form = document.querySelector(".form-group")
+const message= document.querySelector(".message")
+
+form.addEventListener("click", function() {
+    alert("Thank you! Your email address has been added to our mailing list.");
+});
